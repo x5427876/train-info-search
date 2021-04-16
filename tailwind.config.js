9d68ac445b3a387b5a38d6ef91f4939cc5ab6a38
danusorn23456 <40804131+danusorn23456@users.noreply.github.com>
2021-04-16 19:27:55 +0700

    add max-width screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,15 @@ module.exports = {
       './src/**/*.{js,jsx,ts,tsx,vue}',
     ],
     theme: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+      'msm': { 'max': '640px' },
+      'mmd': { 'max': '768px' },
+      'mlg': { 'max': '1024px' },
+      'mxl': { 'max': '1280px' },
+      'm2xl': { 'max': '1536px' },
     }
   }
