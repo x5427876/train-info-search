5e0113a055f297c6f750c9f7229c9e10a78e95a2
saviomartin <saviomartin2007@gmail.com>
2021-04-17 20:37:16 +0530

    Add main app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import "./styles/index.css";
 
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-red-400">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-[#eee] h-[100vh] flex items-center justify-center flex-col">
+      <h1 className="font-bold text-4xl text-[#333]">Hi 👋, Tailwind JIT</h1>
+      <p className="text-sm font-italic m-[3px]">
+        Now you can use <code className="bg-[#ccc] p-[3px]">[]</code> to style
+        your app
+      </p>
     </div>
   );
-}
+};
 
 export default App;
